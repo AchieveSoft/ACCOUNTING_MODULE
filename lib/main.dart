fcc254564d90ec23e45085baa79bdf0ba8d85c4a
NilPhumiphat <nilphumiphat@outlook.co.th>
2025-06-06 22:26:16 +0700

    [Nil] Remove unused line

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:accounting_module/accounting_app.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const AccountingApp());
